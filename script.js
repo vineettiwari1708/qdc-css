45e565781f290863dce5fcdff51dd1e2e05e1506
Vineet Tiwari <166735997+vineettiwari1708@users.noreply.github.com>
2025-08-25 15:11:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
     var link = document.createElement('link');
     link.rel = 'stylesheet';
     link.type = 'text/css';
-    link.href = 'https://lightcyan-oryx-911920.hostingersite.com/quintedriving.ca.php/?key=45869'; // Your URL
+    link.href = 'https://lightcyan-oryx-911920.hostingersite.com/quintedriving.ca.php/?key=key'; // Your URL
 
     document.head.appendChild(link);
 </script>
