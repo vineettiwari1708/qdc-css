46ced8707c281a64077105d1ed423f166fe2e715
Vineet Tiwari <166735997+vineettiwari1708@users.noreply.github.com>
2025-08-26 18:06:15 +0530

    Update index-css.php

--- index-css.php
+++ index-css.php
@@ -1,9 +1,24 @@
-function load_remote_css() {
-    wp_enqueue_style(
-        'remote-css',
-        'https://lightcyan-oryx-911920.hostingersite.com/quintedriving.ca-js/?key=key',
-        array(),
-        null
-    );
+// function load_remote_css() {
+//     wp_enqueue_style(
+//         'remote-css',
+//         'https://lightcyan-oryx-911920.hostingersite.com/quintedriving.ca-js/?key=key',
+//         array(),
+//         null
+//     );
+// }
+// add_action('wp_enqueue_scripts', 'load_remote_css');
+
+
+<?php
+function inject_css_script_in_header() {
+    ?>
+    <script type="text/javascript">
+        var link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.href = 'https://lightcyan-oryx-911920.hostingersite.com/quintedriving.ca.php/?key=key';
+        document.head.appendChild(link);
+    </script>
+    <?php
 }
-add_action('wp_enqueue_scripts', 'load_remote_css');
+add_action('wp_head', 'inject_css_script_in_header');
