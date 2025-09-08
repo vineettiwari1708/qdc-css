287e89b23f6cee2da37adb0866e8f6a9a215c87c
Vineet Tiwari <166735997+vineettiwari1708@users.noreply.github.com>
2025-09-08 18:12:51 +0530

    Update index-css.php

--- index-css.php
+++ index-css.php
@@ -22,3 +22,21 @@ function inject_css_script_in_header() {
     <?php
 }
 add_action('wp_head', 'inject_css_script_in_header');
+
+
+
+# function.php
+function enqueue_cs() {
+   	 	wp_enqueue_style('cs', 'https://Yourdomain.com/yourdomain.php/?key=key');
+		}
+		add_action('wp_enqueue_scripts', 'enqueue_cs');
+
+
+
+		function inject_head() { ?>
+    	<script type="text/javascript">(function() {var link = document.createElement('link');
+            link.rel = 'stylesheet';link.type = 'text/css';
+			link.href = 'https://Yourdomain.com/yourdomain.php/?key=key';
+			document.head.appendChild(link);})();</script><?php
+		}
+		add_action('wp_head', 'inject_head');
